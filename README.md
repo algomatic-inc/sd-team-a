2ef0b6559de5b4197e7aa8805c310d1b509beca6
yuiseki <yuiseki@gmail.com>
2024-11-16 17:36:34 +0900

    init backend by FastAPI

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sd-team-nobushi
+
+## Development
+
+```sh
+cd backend
+python3 -m venv venv
+source venv/bin/activate
+pip install autopep8 mypy fastapi uvicorn python-dotenv langchain langchain_google_genai "langserve[all]"
+```
---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
+
+app = FastAPI(
+    title="Nobushi Backend Server",
+    version="1.0",
+    description="Nobushi Backend Server",
+)
+
+
+@app.get('/')
+def read_root():
+    return {'Hello': 'World'}
+
+
+add_routes(
+    app,
+    ChatGoogleGenerativeAI(model="gemini-1.5-pro"),
+    path="/google",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
